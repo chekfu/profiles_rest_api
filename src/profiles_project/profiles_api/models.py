c055180ccc8c014a41303e3a499ea421089cf4e7
chekfu <chekfu20@gmail.com>
2018-12-14 16:29:26 +0100

    models defined

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,6 +1,14 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    '''Represents a user profile in our system'''
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255,)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+
