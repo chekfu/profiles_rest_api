7c94aa51cd0dee18c18f8ace17c464047d4d99bc
chekfu <chekfu20@gmail.com>
2019-01-31 15:26:44 +0100

    search fields added

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -105,12 +105,12 @@ class HelloViewSet(viewsets.ViewSet):
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    """Handles creating, creating and updateing profiles"""
+    """Handles creating, creating and updating profiles"""
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (permissions.UpdateOwnprofile,)
+    permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
-    search_filters = ('name', 'email')
+    search_fields = ('name', 'email',)
 
