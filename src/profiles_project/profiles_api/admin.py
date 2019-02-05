c781a799341cdf83fa7911a99c4a8e1721c66869
chekfu <chekfu20@gmail.com>
2019-02-05 11:29:49 +0100

    added profile feed model, admin & migrated

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
 
 
--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -53,3 +53,15 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Convert object to a string"""
         return self.email
 
+
+class ProfileFeedItem(models.Model):
+    """Profile status update."""
+
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        """Return model as a string."""
+
+        return self.satus_text
